7d9fab440637eb62ce0ddd098be6417595fc67bb
Micah Leier <nixcamic@gmail.com>
2018-01-02 21:32:10 -0600

    added old site

---
+++ import.rb
@@ -0,0 +1,7 @@
+require "jekyll-import";
+    JekyllImport::Importers::TextPattern.run({
+      "dbname"   => "nixcamic_textpattern",
+      "user"     => "nixcamic-n1x.ca",
+      "password" => "hweiog93bcx",
+      "host"     => "127.0.0.1"
+    })
